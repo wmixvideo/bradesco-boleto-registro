86d2a3752ed5051aa65c74d2a7b371afaeccb7dd
Carine Bockorny <cbockorny@wmixvideo.com.br>
2017-08-17 18:12:23 -0300

    no message

--- src/main/java/com/github/wmixvideo/App.java
+++ src/main/java/com/github/wmixvideo/App.java
@@ -4,8 +4,7 @@ package com.github.wmixvideo;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
